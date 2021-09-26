897de133d19cecb294130a0236e63911801c8c46
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-09-26 09:10:02 +0900

    Specify options

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
 import type { NextPage } from 'next'
 import dynamic from "next/dynamic";
-import React from "react";
+import SimpleMDE from "easymde";
 const SimpleMdeReact = dynamic(import("react-simplemde-editor"), {
   ssr: false,
 });
 
+
 import "easymde/dist/easymde.min.css";
+
+const simpleMDEOptions ={
+    autofocus: true,
+    spellChecker: false,
+    inputStyle: "textarea"
+} as SimpleMDE.Options
 
 const Home: NextPage = () => {
   return (
-    <SimpleMdeReact />
+    <SimpleMdeReact options={simpleMDEOptions}/>
   )
 }
 
